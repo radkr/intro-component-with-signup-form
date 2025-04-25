73b361801ff12cfa2a561545506d7d07ef455e0b
Christian <krisztian.radvanyi@gmail.com>
2025-04-25 18:04:14 +0200

    Bugfix: Specify  subsets for font 'Poppins' to make preload possible.

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 const poppinsSansSerif = Poppins({
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-poppins-sans-serif",
+  subsets: ["latin"],
 });
 
 export const metadata = {
