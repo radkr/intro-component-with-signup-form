037a9206438140893834afb907456e144d5d735a
Christian <krisztian.radvanyi@gmail.com>
2025-04-28 16:57:35 +0200

    Bugfix: I appended assetPrefix to the next.js config so Github Pages to be able to serve the static images  to the client.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "/intro-component-with-signup-form/"
+      : "",
+};
 
 export default nextConfig;
