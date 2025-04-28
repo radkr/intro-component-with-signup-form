334a8afa058e7acb3a798fb33101d7558fb97654
Christian <krisztian.radvanyi@gmail.com>
2025-04-28 17:06:01 +0200

    Bugfix: I appended basePath to the next.js config so Github Pages to be able to serve the static images  to the client.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "/intro-component-with-signup-form/"
-      : "",
+const nextConfig = {};
+
+module.exports = {
+  basePath: "/docs",
 };
 
 export default nextConfig;
