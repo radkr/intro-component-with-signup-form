648549417af4f978ff10575f0605805cc30ccc0c
Christian <krisztian.radvanyi@gmail.com>
2025-04-29 10:17:28 +0200

    Experimental: Modified the next config to leave basePath and assetPrefix unset so I can take a fresh look at how the page looks served by Github Pages with these settings.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/intro-component-with-signup-form",
+  //basePath: "/intro-component-with-signup-form",
 };
 
 export default nextConfig;
